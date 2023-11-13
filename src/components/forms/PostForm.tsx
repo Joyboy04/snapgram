5ec6938602a1dfc2dca57cb9c7781fc95ff9ffa1
Raihan Revi Kasya <raihanrevikasya12@gmail.com>
2023-11-13 12:41:50 +0700

    masih ada fitur yang belom dilanjut

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -10,16 +10,19 @@ import { Textarea } from "../ui/textarea"
 import FileUploader from "../shared/FileUploader"
 import { PostValidation } from "@/lib/validation"
 import { Models } from "appwrite"
-import { useCreatePost } from "@/lib/react-query/queriesAndMutations"
+import { useCreatePost, useUpdatePost } from "@/lib/react-query/queriesAndMutations"
 import { useUserContext } from "@/context/AuthContext"
 import { useToast } from "../ui/use-toast"
 
 type PostFormProps = {
   post?: Models.Document;
+  action: 'Create' | 'Update';
 }
  
-const PostForm = ({ post }: PostFormProps) => {
+const PostForm = ({ post, action }: PostFormProps) => {
   const { mutateAsync: createPost, isPending: isLoadingCreate } = useCreatePost();
+  const { mutateAsync: updatePost, isPending: isLoadingUpdate } = useUpdatePost();
+
   const { user } = useUserContext();
   const { toast } = useToast();
   const navigate = useNavigate();
@@ -37,6 +40,22 @@ const PostForm = ({ post }: PostFormProps) => {
  
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof PostValidation>) {
+    if(post && action === 'Update') {
+      const updatedPost = await updatePost({
+        ...values,
+        postId: post.$id,
+        imageId: post?.imageId,
+        imageUrl: post?.imageUrl,
+      })
+
+      if(!updatedPost) {
+        toast({ title: 'Please try again' })
+      }
+
+      return navigate(`/posts/${post.$id}`)
+    }
+
+
     const newPost = await createPost({
       ...values,
       userId: user.id,
@@ -50,6 +69,7 @@ const PostForm = ({ post }: PostFormProps) => {
 
     navigate('/');
   }
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
@@ -118,10 +138,13 @@ const PostForm = ({ post }: PostFormProps) => {
           className="shad-button_dark_4">Cancel
           </Button>
 
-          <Button 
-          type="submit"
-          className="shad-button_primary whitespace-nowrap">Submit
-          </Button>
+        <Button 
+        type="submit" 
+        className="shad-button_primary"
+        disabled={isLoadingCreate || isLoadingUpdate}>
+          {isLoadingCreate || isLoadingUpdate && 'loading...'}
+          {action} Post
+      </Button>
         </div>
       </form>
     </Form>
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -58,19 +58,16 @@ import {
   export const useGetPosts = () => {
     return useInfiniteQuery({
       queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-      queryFn: getInfinitePosts as any,
-      getNextPageParam: (lastPage: any) => {
-        // If there's no data, there are no more pages.
-        if (lastPage && lastPage.documents.length === 0) {
-          return null;
-        }
-      
-        // Use the $id of the last document as the cursor.
-        const lastDocument = lastPage.documents[lastPage.documents.length - 1];
-        return lastDocument && lastDocument.$id;
-      },
-    });
-  };
+      queryFn: getInfinitePosts,
+      getNextPageParam: (lastPage) => {
+        if(lastPage && lastPage.documents.length === 0) return null;
+
+        const lastId = lastPage.documents[lastPage?.documents.length - 1].$id;
+
+        return lastId;
+      }
+    })
+  }
   
   export const useSearchPosts = (searchTerm: string) => {
     return useQuery({
